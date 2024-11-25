da2b6e4f9d5dccd4cfd2f8c1776671ea9a1b607c
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2024-11-25 17:54:01 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import purgecss from "vite-plugin-purgecss";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/krishi-vikas-udyog/',
+  plugins: [
+    react(),
+    purgecss({
+      content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"], // Specify your files
+    }),
+  ],
+  base: "/krishi-vikas-udyog/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-  
-})
+});
