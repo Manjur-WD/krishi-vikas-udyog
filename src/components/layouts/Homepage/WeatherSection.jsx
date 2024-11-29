71cd925ff5a8e72bbbd8f33c3cb4a79f8c891038
Manjur-WD <mak985812@gmail.com>
2024-11-30 01:52:46 +0530

    updated

--- src/components/layouts/Homepage/WeatherSection.jsx
+++ src/components/layouts/Homepage/WeatherSection.jsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
 import { getWeatherData } from "../../../services/api";
 import { useQuery } from "@tanstack/react-query";
+import humidity from "../../../assets/humidity.svg";
+import airpressure from "../../../assets/airpressure.svg";
+import feelslike from "../../../assets/feelslike.svg";
+import windspeed from "../../../assets/windspeed.svg";
+
+
 
 const WeatherSection = () => {
 
   const now = new Date();
+  console.log(now);
+
 
   // Get the day of the week (e.g., "Monday", "Tuesday", etc.)
   const dayOfWeek = now.toLocaleString("en-US", { weekday: "long" });
 
-  // Get the current time in HH:MM:SS format
-  const time = now.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  // Get the current date in day-month-year format
+  const date = now.toLocaleDateString("en-GB");
 
-  console.log(`Today is ${dayOfWeek} and the current time is ${time}`);
+  // console.log(`Today is ${dayOfWeek} and the current time is ${date}`);
 
   const { data: weatherData } = useQuery({
     queryKey: ["weather-data"],
@@ -26,6 +31,10 @@ const WeatherSection = () => {
   const [weatherStatus, setWeatherStatus] = useState("");
   const [currentTemp, setCurrentTemp] = useState("");
   const [currentCity, setCurrentCity] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [airpressure, setAirpressure] = useState("");
+  const [feelslikw, setFeelslike] = useState("");
+  const [windspeed, setwindSpeed] = useState("");
 
   useEffect(() => {
     if (weatherData) {
@@ -33,6 +42,10 @@ const WeatherSection = () => {
       setWeatherStatus(weatherData.data.current[0].main);
       setCurrentTemp(weatherData.data.current[0].temp);
       setCurrentCity(weatherData.data.city_name);
+      setHumidity(weatherData.data.current[0].humidity);
+      setAirpressure(weatherData.data.current[0].pressure);
+      setFeelslike(weatherData.data.current[0].feels_like);
+      setwindSpeed(weatherData.data.current[0].wind_speed);
     }
   }, [weatherData]);
 
@@ -41,22 +54,33 @@ const WeatherSection = () => {
   return (
     <>
       <section className="weather-section-homepage bg-weatherHomepage bg-cover bg-center">
-        <div className="container grid grid-cols-2">
-          <div className="current-status-weather flex">
-            <img src={weatherIcon} alt="weather-icon" className="w-[200px]" />
-            <div className="w-current-details">
+        <div className="container grid md:grid-cols-2 grid-cols-1">
+          <div className="current-status-weather flex nd:gap-5 md:flex-row flex-col justify-center items-center">
+            <img src={weatherIcon} alt="weather-icon" className="w-[150px]  drop-shadow-2xl -mt-[50px]" />
+            <div className="w-current-details md:text-start text-center">
               <p className="text-white text-2xl shadow-lg font-semibold">
                 {weatherStatus}
               </p>
+              <p className="text-xl text-white">{dayOfWeek}</p>
+              <p className="text-xl text-white font-light">{date}</p>
               <p className="text-7xl font-bold text-white">
                 {currentTemp}
                 <span className="ms-2">°C</span>
               </p>
-              <p className="text-2xl text-white">{dayOfWeek}</p>
-              <p className="text-2xl text-white">{time}</p>
+
             </div>
           </div>
-          <div className="additionals-weather"></div>
+          <div className="additionals-weather grid md:grid-cols-4 grid-cols-2">
+            <div className="additional-weather-data-box">
+              <p className="text-xl text-white uppercase"><img src={humidity} alt="icon of weather" className="invert inline me-2" />humidity</p>
+              <p className="text-2xl font-bold">
+                {humidity}
+              </p>
+            </div>
+            <p><img src={humidity} alt="icon of weather" className="invert" />humidity</p>
+              <p><img src={humidity} alt="icon of weather" className="invert" />humidity</p>
+              <p><img src={humidity} alt="icon of weather" className="invert" />humidity</p>
+          </div>
         </div>
       </section>
     </>
