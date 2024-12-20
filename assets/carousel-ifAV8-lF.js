1c567e5d2a3568593e3231a18656c6a2c927732c
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2024-12-20 19:35:12 +0530

    Updates

--- assets/carousel-ifAV8-lF.js
+++ assets/carousel-ifAV8-lF.js
@@ -1,4 +1,4 @@
-import{c as Kt,r as D,j as R,h as gt}from"./index-BHvSYwXL.js";import{B as Ut}from"./button-B-R53LFt.js";/**
+import{c as Kt,r as D,j as R,h as gt}from"./index-CrU52Nxm.js";import{B as Ut}from"./button-CcNCVrqB.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
