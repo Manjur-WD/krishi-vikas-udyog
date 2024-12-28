387c15eb01f85429d5ba50790e8d6b9b123b1006
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2024-12-28 19:28:12 +0530

    Updates

---
+++ assets/SellProductPage-CfGY6vjX.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-B7wwjzwm.js";import{H as e,F as s}from"./Header-JgC6DDL2.js";const a=()=>r.jsxs(r.Fragment,{children:[r.jsx(e,{}),r.jsx("h1",{children:"This is Product Selling Page"}),r.jsx(s,{})]});export{a as default};
