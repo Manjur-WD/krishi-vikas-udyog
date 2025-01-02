e004f7004777d66bd0758bf69df0bfd9efb20404
Manjur-WD <mak985812@gmail.com>
2025-01-02 22:21:29 +0530

    updated

--- src/components/layouts/Header/sections/MiddleHeadSection.jsx
+++ src/components/layouts/Header/sections/MiddleHeadSection.jsx
@@ -112,13 +112,9 @@ const MiddleHeadSection = () => {
             </a>
           </div>
           <div className="header__wishlist-login items-center flex">
-<<<<<<< HEAD
-            
-=======
-            <button type="button" className="md:hidden inline border-transparent hover:border-gray-200 hover:scale-95 px-4 py-1">
-              <LanguageSelector className="md:hidden block" />
+            <button className="language-selector md:hidden inline">
+              <LanguageSelector />
             </button>
->>>>>>> 69f9cbd3086691cb715b2b2c59d463a2165345d8
             <button
               type="button"
               className=" hover:scale-95 md:px-4 py-1 relative md:block hidden cursor-pointer"
@@ -145,7 +141,7 @@ const MiddleHeadSection = () => {
                   <div className="user-button-wrapper">
                     <div className="user-button border border-dashed border-transparent hover:border-gray-200 hover:scale-95 px-4 py-1">
                       <PiUserCircleDashedFill className="me-2 inline align-bottom text-3xl text-lightgreen" />
-                      <span className="text-lg">User</span>
+                      <span className="md:text-lg text-sm">User</span>
                     </div>
                     <ul className="user-menus border shadow animate__animated animate__fadeIn animate__faster">
                       <li><ImProfile className="me-2 inline" />My Profile</li>
@@ -156,7 +152,7 @@ const MiddleHeadSection = () => {
                 ) :
                 (
                   <Dialog >
-                    <DialogTrigger className="border border-dashed border-transparent hover:border-gray-200 hover:scale-95 md:px-4 py-1">
+                    <DialogTrigger className="border border-dashed border-transparent hover:border-gray-200 hover:scale-95 md:px-4 px-2 py-1">
                       <PiUserCircleDashedFill className="me-1 inline align-bottom text-3xl text-lightgreen" />
                       <span className="md:text-lg inline-block pb-1">{t('Login')}</span>
                     </DialogTrigger>
