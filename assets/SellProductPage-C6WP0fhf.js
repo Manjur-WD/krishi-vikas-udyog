bf607fcd5cdce05cc14afbd8e60d0a7bb38ed3f3
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2025-01-07 18:36:21 +0530

    Updates

---
+++ assets/SellProductPage-C6WP0fhf.js
@@ -0,0 +1,1 @@
+import{j as e,K as s,N as r}from"./index-DrUYU_1W.js";const a=()=>e.jsxs(e.Fragment,{children:[e.jsx(s,{}),e.jsx("h1",{children:"This is Product Selling Page"}),e.jsx(r,{})]});export{a as default};
--- assets/carousel-ByMndD8q.js
+++ assets/carousel-ByMndD8q.js
@@ -1,4 +1,4 @@
-import{c as Kt,r as D,j as R,h as gt,aB as Ut}from"./index-tmDpJ0aC.js";/**
+import{c as Kt,r as D,j as R,h as gt,aB as Ut}from"./index-DrUYU_1W.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
